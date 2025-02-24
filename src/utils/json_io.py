83b3889adba05ea22ec909b74ef0eb88ee7a01fb
Rajakumaran Arivumani <10078107+dexplorer@users.noreply.github.com>
2025-02-24 21:19:01 +0000

    add json io method

--- src/utils/json_io.py
+++ src/utils/json_io.py
@@ -1,6 +1,24 @@
+import json 
+import logging 
+from utils import file_io as uff 
+
+def uf_read_json_file_to_list_of_dict(file_path: str) -> list[dict]:
+    with uff.uf_open_file(file_path=file_path, open_mode="r") as f:
+        file_records: list[dict] = json.load(f)
+
+    try:
+        if file_records:
+            # print(file_records[:2])
+            return file_records
+        else:
+            raise ValueError("Error in reading the file.")
+    except ValueError as error:
+        logging.error(error)
+        raise
+
 def uf_write_list_of_data_cls_obj_to_json_file(obj_list: list, file_path: str):
     try:
-        if dataclass_obj_list:
+        if obj_list:
             json_str = json.dumps(obj_list, default=lambda x: x.__dict__)
         else:
             raise RuntimeError("No data in the dataclass object list.")
